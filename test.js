e8d29d4020f2b2db46e3130276f27ce79b3cd9db
origin <shell@mail.lan>
2024-06-08 16:40:18 +0400

    add test

---
+++ test.js
@@ -0,0 +1,23 @@
+async function measureFetchPerformance(numRequests) {
+  const start = Date.now();
+  const requests = [];
+
+  for (let i = 0; i < numRequests; i++) {
+    requests.push(
+      fetch('http://localhost:3000/hello-world'));
+  }
+
+  await Promise.all(requests);
+
+  const end = Date.now();
+  const totalTime = end - start;
+  const averageTime = totalTime / numRequests;
+
+  return {
+    totalRequests: numRequests,
+    totalTime,
+    averageTime
+  };
+}
+
+measureFetchPerformance(1000000).then(console.log).catch(console.error);

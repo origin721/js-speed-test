52b5d8afb781a08099122627bb8d648d3476723c
origin <shell@mail.lan>
2024-06-09 06:01:27 +0400

    release/v4.0.3

--- nodejs/test1.js
+++ nodejs/test1.js
@@ -40,60 +40,54 @@ async function main() {
     }
 
 
-    let counterResolve = 48; // (2 * 6) + (3*6) + (3*6)
+    let counterResolve = 18; // 6 * 3
 
     for (let j = 0; j < 6; ++j) {
 
-        for (let z = 0; z < 2; ++z) {
-            setTimeout(async () => {
-                for (let i = 0; i < 10; ++i) {
-                    const localPromises = [];
-                    localPromises.push(measurePerformance({
-                        url: hostServer + '/hello-world',
-                        method: 'GET',
-                        data: Array.from({ length: 1000 }).fill(undefined),
-                    }));
-                    localPromises.forEach(el => el.then(console.log))
-                    listPromises.concat(localPromises);
-                    await Promise.allSettled(localPromises)
-                }
-                resolveRequest();
-            })
-        }
+        setTimeout(async () => {
+            for (let i = 0; i < 10; ++i) {
+                const localPromises = [];
+                localPromises.push(measurePerformance({
+                    url: hostServer + '/hello-world',
+                    method: 'GET',
+                    data: Array.from({ length: 1000 }).fill(undefined),
+                }));
+                localPromises.forEach(el => el.then(console.log))
+                listPromises.concat(localPromises);
+                await Promise.allSettled(localPromises)
+            }
+            resolveRequest();
+        })
 
-        for (let z = 0; z < 3; ++z) {
-            setTimeout(async () => {
-                for (let i = 0; i < 10; ++i) {
-                    const localPromises = [];
-                    localPromises.push(measurePerformance({
-                        url: hostServer + '/concat-strings',
-                        method: 'POST',
-                        data: listString,
-                    }));
-                    localPromises.forEach(el => el.then(console.log))
-                    listPromises.concat(localPromises);
-                    await Promise.allSettled(localPromises)
-                }
-                resolveRequest();
-            })
-        }
+        setTimeout(async () => {
+            for (let i = 0; i < 10; ++i) {
+                const localPromises = [];
+                localPromises.push(measurePerformance({
+                    url: hostServer + '/concat-strings',
+                    method: 'POST',
+                    data: listString,
+                }));
+                localPromises.forEach(el => el.then(console.log))
+                listPromises.concat(localPromises);
+                await Promise.allSettled(localPromises)
+            }
+            resolveRequest();
+        })
 
-        for (let z = 0; z < 3; ++z) {
-            setTimeout(async () => {
-                for (let i = 0; i < 10; ++i) {
-                    const localPromises = [];
-                    localPromises.push(measurePerformance({
-                        url: hostServer + '/array-sum',
-                        method: 'POST',
-                        data: listNumbers,
-                    }));
-                    localPromises.forEach(el => el.then(console.log))
-                    listPromises.concat(localPromises);
-                    await Promise.allSettled(localPromises)
-                }
-                resolveRequest();
-            })
-        }
+        setTimeout(async () => {
+            for (let i = 0; i < 10; ++i) {
+                const localPromises = [];
+                localPromises.push(measurePerformance({
+                    url: hostServer + '/array-sum',
+                    method: 'POST',
+                    data: listNumbers,
+                }));
+                localPromises.forEach(el => el.then(console.log))
+                listPromises.concat(localPromises);
+                await Promise.allSettled(localPromises)
+            }
+            resolveRequest();
+        })
     }
 
     //listPromises.forEach(el => el.then(console.log))

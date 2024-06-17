21724fde910fe50e0cd2a03853e3c3466c783ab6
shlavik <shlavik@gmail.com>
2024-06-17 19:52:34 +0400

    rust server added

---
+++ rust/README.md
@@ -0,0 +1,7 @@
+## Simple Service
+
+# TO START SERVICE
+> cargo run
+
+# REQUEST EXAMPLE
+> curl -X POST http://127.0.0.1:3000/ -H 'Content-Type: application/json' -d '[2.88, 3.1, 4.2]'
---
+++ rust/src/main.rs
@@ -0,0 +1,17 @@
+use axum::{extract::Json, routing::post, Router};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+async fn sum_numbers(Json(numbers): Json<Vec<f64>>) -> Json<f64> {
+    let sum: f64 = numbers.iter().sum();
+    Json(sum)
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", post(sum_numbers));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("SERVICE STARTED @ http://{}", addr);
+    let listener = TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
